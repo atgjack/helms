9f446e06dc3381a6bb41da28059479eb93154958
atgJack <jack@atg.io>
2016-11-11 19:25:42 -0500

    better bundling

---
+++ dist/lib.es.js
@@ -0,0 +1,75 @@
+function copy(obj) {
+  var clone = void 0;
+
+  // Return literals.
+  if (!(obj instanceof Object)) return obj;
+
+  // Create clone object with proper params.
+  var Constructor = obj.constructor;
+  switch (Constructor) {
+    case Date:
+    case RegExp:
+      clone = new Constructor(obj);
+      break;
+    default:
+      clone = new Constructor();
+  }
+
+  // Copy each property.
+  for (var property in obj) {
+    clone[property] = copy(obj[property]);
+  }return clone;
+}
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function compare(a, b) {
+
+  // Check same literal value or same object reference.
+  if (a === b) return true;
+  if (a === null || b === null) return false;
+
+  // Check types and short-circit on non-objects.
+  var type = typeof a === 'undefined' ? 'undefined' : _typeof(a);
+  if (type !== (typeof b === 'undefined' ? 'undefined' : _typeof(b))) return false;
+  if (type === 'number' && isNaN(a) && isNaN(b)) return true;
+  if (type !== 'object') return a === b;
+  if (checkInstance(a, b)) return a.toString() === b.toString();
+
+  // Check object equivalence.
+  if (a.isPrototypeOf(b) || b.isPrototypeOf(a)) return false;
+  if (a.constructor !== b.constructor) return false;
+  if (a.prototype !== b.prototype) return false;
+  if (Object.keys(a).length !== Object.keys(b).length) return false;
+  for (var property in a) {
+    if (!compare(a[property], b[property])) return false;
+  }
+
+  // Must be the same.
+  return true;
+}
+
+// Check for objects that allow string comparisons.
+function checkInstance(a, b) {
+  return a instanceof Date && b instanceof Date || a instanceof RegExp && b instanceof RegExp || a instanceof String && b instanceof String || a instanceof Number && b instanceof Number;
+}
+
+var constructors = [Date, RegExp, String, Number];
+
+function merge(a, b) {
+  if (!(a instanceof Object) || !(b instanceof Object)) throw new TypeError('You can only merge objects.');
+
+  for (var property in b) {
+    var oldProp = a[property];
+    var newProp = b[property];
+    if (compare(oldProp, newProp)) continue;
+    if (constructors.indexOf(newProp.constructor) !== -1) {
+      a[property] = newProp;
+    } else if (newProp instanceof Object && oldProp instanceof Object) {
+      merge(oldProp, newProp);
+    } else a[property] = newProp;
+  }
+}
+
+export { copy, compare, merge };
+//# sourceMappingURL=lib.es.js.map
--- rollup.js
+++ rollup.js
@@ -2,37 +2,38 @@ var rollup =  require('rollup');
 var babel  = require('rollup-plugin-babel');
 var uglify = require('rollup-plugin-uglify');
 
+var pkg = require('./package.json');
+var externals = Object.keys(pkg.dependencies || {});
+
 const babelConfig = {
   babelrc: false,
   presets: [
     ["es2015", { modules: false }],
     "stage-0"
-  ],
-  plugins: [ "external-helpers" ]
+  ]
 }
 
 rollup.rollup({
   entry: 'lib/index.js',
-  plugins: [ babel(babelConfig) ],
+  external: externals,
+  plugins: [ babel(babelConfig) ]
 }).then( function(bundle) {
   bundle.write({
     format: 'es',
-    dest: 'dist/lib.es6.js'
-  });
-  bundle.write({
-    format: 'umd',
-    moduleName: 'helms',
-    dest: 'dist/lib.js'
+    sourceMap: true,
+    dest: pkg['jsnext:main']
   });
 }).catch( function(err) { console.log(err) });
 
 rollup.rollup({
   entry: 'lib/index.js',
+  external: externals,
   plugins: [ babel(babelConfig), uglify() ],
 }).then( function(bundle) {
   bundle.write({
     format: 'umd',
     moduleName: 'helms',
-    dest: 'dist/lib.min.js'
+    sourceMap: true,
+    dest: pkg['main']
   });
 }).catch( function(err) { console.log(err) });
